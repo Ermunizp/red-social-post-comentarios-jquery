7a8cb6406e62430c18789a3263bdcfe69ba00ae7
Ermunizp <ermuniz@espe.edu.ec>
2025-01-20 16:33:05 -0500

    Update comments.js

--- js/comments.js
+++ js/comments.js
@@ -1,6 +1,6 @@
 // comments.js
 
-// Function to add a comment to a specific post
+// Función para añadir un comentario a un post específico
 const addComment = (postIndex, commentText) => {
     if (!posts[postIndex].comments) {
         posts[postIndex].comments = [];
@@ -9,7 +9,7 @@ const addComment = (postIndex, commentText) => {
     displayPosts();
 };
 
-// Function to delete a comment from a specific post
+// Función para eliminar un comentario de un post específico
 const deleteComment = (postIndex, commentIndex) => {
     if (posts[postIndex].comments && posts[postIndex].comments.length > commentIndex) {
         posts[postIndex].comments.splice(commentIndex, 1);
